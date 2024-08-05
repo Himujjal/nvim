f0fcca28016b038baaecf82e4ccaa729da0487d7
Himujjal Upadhyaya <himu@tuta.io>
2024-08-05 15:33:08 +0530

    astro v4 second commit

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,9 +1,11 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
+
+local buffer_close_picker = function()
+  require("astroui.status").heirline.buffer_picker(function(bufnr) require("astrocore.buffer").close(bufnr) end)
+end
 
 ---@type LazySpec
 return {
@@ -42,30 +44,31 @@ return {
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
+
       -- first key is the mode
       n = {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["}"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["{"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["zz"] = { "<cmd>:w!<cr>", desc = "Save files" },
+        ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
 
         -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
+        ["<Leader>bD"] = { buffer_close_picker, desc = "Pick to close" },
+        ["<Leader>b"] = { name = "Buffers" },
+      },
+      v = {
+        -- search in visual mode
+        ["/"] = { [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], silent = true, desc = "Search in visual mode" },
+
+        ["<leader>ge"] = {
+          [[:<BS><BS><BS><BS><BS>ChatGPTEditWithInstructions<cr>]],
+          desc = "ChatGPT Instruction for the code",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<C-g>t"] = { ":GpChatToggle popup<cr>", desc = "Toggle Chat (Visual)" },
       },
     },
   },
---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,55 @@
+local env = require "env"
+
+local API_KEY = env.OPENAI_KEY
+local MODEL = "gpt-3.5-turbo"
+local API_HOST = "https://api.openai.com"
+
+-- Calling this function is more than enough to enable Groq
+local enableGroq = function()
+  API_HOST = "https://api.groq.com/openai"
+  API_KEY = env.GROQ_KEY
+  MODEL = "llama3-8b-8192"
+  -- MODEL = "mixtral-8x7b-32768"
+end
+
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    -- enableGroq() -- comment/uncomment this line to use Groq
+
+    vim.env.OPENAI_API_KEY = API_KEY
+    vim.env.OPENAI_API_HOST = API_HOST
+
+    require("chatgpt").setup {
+      keymaps = {
+        close = "q",
+        delete_session = "<leader>dd",
+      },
+      api_key_cmd = "echo $OPENAI_API_KEY",
+      openai_params = {
+        model = MODEL,
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 3000,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = MODEL,
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    }
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
